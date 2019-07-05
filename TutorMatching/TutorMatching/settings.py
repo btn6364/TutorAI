3b060c0c6d9a9a356e2c5bc85e3f430ec5712840
winkhanh <giakhanhnk2000@gmail.com>
2019-07-06 00:02:28 +0700

    Init react

--- TutorMatching/TutorMatching/settings.py
+++ TutorMatching/TutorMatching/settings.py
@@ -39,9 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    "rest_framework",
+    "corsheaders", 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # added to solve CORS
+    'django.middleware.common.CommonMiddleware','
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True # added to solve CORS
