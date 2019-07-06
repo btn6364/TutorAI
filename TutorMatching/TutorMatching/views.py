5ef9836a7b2738085df582880abe4e3cc23bd3da
Bao Nguyen <btn6364@rit.edu>
2019-07-06 02:14:27 -0400

    get tutors functionality

--- TutorMatching/TutorMatching/views.py
+++ TutorMatching/TutorMatching/views.py
@@ -2,7 +2,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-
 # This will return a list of books
 @api_view(["GET"])
 def book(request):
--- TutorMatching/tutordata/views.py
+++ TutorMatching/tutordata/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import Tutor
+from django.core import serializers
+@api_view(["GET"])
+def tutors(request):
+    tutors = serializers.serialize("json", Tutor.objects.all())
+    return Response(status=status.HTTP_200_OK, data={"data": tutors})
+
