d439571d2ed69cd903c92e544360f3aa0100625d
Bao Nguyen <btn6364@rit.edu>
2019-07-06 22:00:29 -0400

    change

--- frontend/src/Components/Tutor/Tutor.js
+++ frontend/src/Components/Tutor/Tutor.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Tutor.css';
+
+const locations = ['Da Nang', 'Ha Noi', 'Ho Chi Minh']
+const subjects = ['Math', 'English', 'Physics']
 const Tutor = (props)=>{
     return(
         <div className="card tutor-component" >
@@ -7,11 +10,11 @@ const Tutor = (props)=>{
                 <h5 className="card-title">{`${props.tutor.first_name} ${props.tutor.last_name} `}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{props.tutor.email}</h6>
                 <p className="card-text">
-                    Location : {props.tutor.location} <br></br>
-                    Subject: {props.tutor.subject} <br></br>
+                    Location : {locations[props.tutor.location-1]} <br></br>
+                    Subject: {subjects[props.tutor.subject-1]} <br></br>
                     Price: {props.tutor.price}/hour
                 </p>
-                
+
             </div>
         </div>
     )
